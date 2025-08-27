5e96301441b00498d77c0226997279361520ec10
Ryuukomo <98666355+Ryuukomo@users.noreply.github.com>
2025-08-27 17:16:45 -0300

    animação samurai e contagem de texto na tela

--- Assets/Scripting/jogo1/Player.cs
+++ Assets/Scripting/jogo1/Player.cs
@@ -1,21 +1,32 @@
+using System.Drawing;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public Transform projetil;
+    TextMeshProUGUI texto;
+    TextMeshProUGUI texto2;
+    Transform projetil;
     public float velocidade = 0;
     public float y; 
     public float x;
     public int n = 0;
+    int vida;
     public bool estaDireita = true;
     Vector3 vect;
 
 
     void Start()
     {
+        texto = GameObject.Find("Biscoito").transform.GetComponent<TextMeshProUGUI>();
+        texto2 = GameObject.Find("Vida").transform.GetComponent<TextMeshProUGUI>();
+        projetil = GameObject.Find("Tiro").transform;
+      vida = 4;
        vect = transform.position;   
+        texto2.text = "Vida: <color=green> " + vida + " </color> ";
+
     }
 
     // Update is called once per frame
@@ -27,17 +38,21 @@ public class Player : MonoBehaviour
         if (movimente == 1)
         {
             transform.eulerAngles = new Vector2(0, 0);
+            estaDireita = true;
+            //Instanciado.GetComponent<Projetil>().direcao = new Vector2(1, 0);
         }
 
         if (movimente == - 1)
         {
             transform.eulerAngles = new Vector2(0, 180);
+            estaDireita = false;
         }
      if (Input.GetKeyDown(KeyCode.T) == true )
         {
             Transform Instanciado = Instantiate(projetil);
             Instanciado.position = transform.position;
             Instanciado.GetComponent<Projetil>().enabled = true;
+
 
             if (estaDireita == true)
             {
@@ -54,26 +69,39 @@ public class Player : MonoBehaviour
         float pulo = Input.GetAxisRaw("Vertical") * Time.deltaTime;
         
         transform.position += new Vector3(movimente * velocidade, pulo * velocidade);
-        
-        
        
+        
+    
+
 
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+      
         if (collision.CompareTag("Abismo"))
         {
             transform.position = vect;
             n = 0;
+           
             Debug.Log("Parabéns !! Você pegou:");
-         
+            vida--;
+            if (vida == 1)
+            {
+                
+               texto2.text = "Vida: <color=red> " + vida + " </color> ";
+            }
+            
         }
         if (collision.gameObject.name.Contains("Biscoito") == true)
         {
+
             Destroy(collision.gameObject);
+            
             Debug.Log("Parabéns !! Você pegou:" + n++);
+            
+            texto.text = "Biscoito: <color=yellow>"+ n++ +"</color>"; 
         }
     }
 
